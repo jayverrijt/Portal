ee66244de94c5f04c484b85be9475e4b5fab58ce
Jay Verrijt <jay@famverrijt.nl>
2026-09-14 09:45:47 +0200

    css fix: notes overview

--- Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
+++ Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e84df3bc9d78a416021d42cb8801b03bbe8ca3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee4c575c714f0c9b201e68e16676770d02be92f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
